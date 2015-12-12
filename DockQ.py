bb0b7d6164d51e3f4e70139538ea8105d5a16138
Björn Wallner <bjornwallner@gmail.com>
2015-12-12 15:40:56 +0100

    Fixed LRMS

--- DockQ.py
+++ DockQ.py
@@ -4,7 +4,9 @@ import Bio.PDB
 import sys
 import os
 import re
+import numpy as np
 from Bio.SVDSuperimposer import SVDSuperimposer
+from math import sqrt
 
 def parse_fnat(fnat_out):
     fnat=0;
@@ -16,6 +18,8 @@ def parse_fnat(fnat_out):
         if(re.search(r'^Fnat',line)):
             list=line.split(' ')
             fnat=list[3]
+            nat_correct=list[1]
+            nat_total=list[2]
         elif(match):
             #print line
             res1=match.group(1)
@@ -25,7 +29,20 @@ def parse_fnat(fnat_out):
            # print res1 + ' ' + chain1 + ' ' + res2 + ' ' + chain2
             inter.append(res1 + chain1)
             inter.append(res2 + chain2)
-    return (fnat,inter)
+    return (fnat,nat_correct,nat_total,inter)
+
+def capri_class(fnat,iRMS,LRMS):
+
+    if(fnat < 0.1 or (LRMS > 10 and iRMS>=4.0)):
+        return 'Incorrect'
+    elif((fnat >=0.1 and fnat <0.3) and (LRMS <=10 or iRMS <=4) or (fnat >= 0.3 and LRMS > 5 and iRMS > 2)):
+        return 'Acceptable'
+    elif((fnat >=0.3 and fnat <0.5) and (LRMS <=5 or iRMS <= 2) or (fnat >=0.5 and LRMS >1 and iRMS > 1)):
+        return 'Medium'
+    elif(fnat >=0.5 and (LRMS < 1 or iRMS < 1)):
+        return 'High'
+    else:
+        return 'Undef'
 
 exec_path=os.path.dirname(os.path.abspath(sys.argv[0]))
 model=sys.argv[1]
@@ -35,7 +52,7 @@ cmd=exec_path + '/fnat ' + model + ' ' + native + ' 5'
 
 
 fnat_out = os.popen(cmd).readlines()
-(fnat,interface)=parse_fnat(fnat_out)
+(fnat,nat_correct,nat_total,interface)=parse_fnat(fnat_out)
 #print inter_pairs
 #fnat=grep(re('^Fnat',fnat_out))
 
@@ -144,9 +161,15 @@ irms=super_imposer.rms
 
 ligand_chain=chain1
 receptor_chain=chain2
+len1=len(chain_sample[chain1])
+len2=len(chain_sample[chain2])
+class1='ligand'
+class2='receptor'
 if(len(chain_sample[chain1]) > len(chain_sample[chain2])):
     receptor_chain=chain1
     ligand_chain=chain2
+    class1='receptor'
+    class2='ligand'
 
 
 #print chain_sample.keys()
@@ -154,18 +177,54 @@ if(len(chain_sample[chain1]) > len(chain_sample[chain2])):
 #Set to align on receptor
 super_imposer.set_atoms(chain_ref[receptor_chain], chain_sample[receptor_chain])
 super_imposer.apply(sample_model.get_atoms())
+receptor_chain_rms=super_imposer.rms
+#print receptor_chain_rms
 #print dir(super_imposer)
 #print chain1_rms
-#Set to calculate rms on ligand
-super_imposer.set_atoms(chain_ref[ligand_chain], chain_sample[ligand_chain])
-Lrms=super_imposer.rms
+
+#Grep out the transformed ligand coords
+
+coord1=np.array([atom.coord for atom in chain_ref[ligand_chain]])
+coord2=np.array([atom.coord for atom in chain_sample[ligand_chain]])
+
+#coord1=np.array([atom.coord for atom in chain_ref[receptor_chain]])
+#coord2=np.array([atom.coord for atom in chain_sample[receptor_chain]])
+sup=SVDSuperimposer()
+Lrms = sup._rms(coord1,coord2) #using the private _rms function which does not superimpose
+
+
+#super_imposer.set_atoms(chain_ref[ligand_chain], chain_sample[ligand_chain])
+#super_imposer.apply(sample_model.get_atoms())
+#coord1=np.array([atom.coord for atom in chain_ref[receptor_chain]])
+#coord2=np.array([atom.coord for atom in chain_sample[receptor_chain]])
+#Rrms= sup._rms(coord1,coord2)
+#should give same result as above line
+#diff = coord1-coord2
+#l = len(diff) #number of atoms
+#from math import sqrt
+#print sqrt(sum(sum(diff*diff))/l)
+#print np.sqrt(np.sum(diff**2)/l) 
 
 DockQ=(float(fnat) + 1/(1+(irms/1.5)*(irms/1.5)) + 1/(1+(Lrms/8.5)*(Lrms/8.5)))/3
-
-print 'Fnat ' + fnat
+print '*** DockQ and other docking quality measures *** '
+print 'Number of equivalent residues in chain ' + chain1 + ' ' + str(len1) + ' (' + class1 + ')'
+print 'Number of equivalent residues in chain ' + chain2 + ' ' + str(len2) + ' (' + class2 + ')'
+print 'Fnat ' + fnat + ' ' + nat_correct + ' correct of ' + nat_total
 print 'iRMS ' + str(irms)
-print 'LRMS ' + str(Lrms)
+print 'LRMS ' + str(Lrms) #+ ' Ligand RMS'
+#print 'RRMS ' + str(Rrms) + ' Receptor RMS'
+print 'CAPRI ' + capri_class(fnat,irms,Lrms)
 print 'DockQ ' + str(DockQ)
+
+#for f in np.arange(0,1.01,0.2):
+#    
+#    for i in np.arange(0.5,5.5,1):
+#    
+#        for L in np.arange(0.5,13.5,3):
+#            print str(f) + ' ' + str(i) + ' ' + str(L) + ' ' + capri_class(f,i,L)
+#
+
+
 # Save the aligned version of 1UBQ.pdb
 #io = Bio.PDB.PDBIO()
 #io.set_structure(sample_structure) 
--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # DockQ
-Requires Biopython
+Requires `Biopython` and `numpy` 
 
 Compile using `make`
 
 Run with
 `./DockQ.py <model> <native>`
+
+Example
+`bash$ ./DockQ.py model.pdb native.pdb
+*** DockQ and other docking quality measures *** 
+Number of equivalent residues in chain A 373 (receptor)
+Number of equivalent residues in chain B 228 (ligand)
+Fnat 0.533333 32 correct of 60
+iRMS 1.15759298174
+LRMS 1.50152873613
+CAPRI Medium
+DockQ 0.70993637399`
