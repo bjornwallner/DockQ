947d6e3da80e70a332b4d9723baf6ecb73b17fc8
Björn Wallner <bjornwallner@gmail.com>
2016-06-21 08:07:29 +0200

    ex

--- DockQ.py
+++ DockQ.py
@@ -483,7 +483,9 @@ def main():
 #    print native_chains
     if((len(model_chains) > 2 or len(native_chains) > 2) and
        (args.model_chain1 == None and args.native_chain1 == None)):
-        print "Multi-chain model need sets of chains to group\nuse -model_chain1 and/or -native_chain1"
+        print "Multi-chain model need sets of chains to group\nuse -native_chain1 and/or model_chain1 if you want a different mapping than 1-1"
+        print "Model chains  : " + str(model_chains)
+        print "Native chains : " + str(native_chains)
         sys.exit()
     if not args.skip_check and (len(model_chains) < 2 or len(native_chains)< 2):
         print "Need at least two chains in the two inputs\n";
