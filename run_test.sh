2e298d3a339d2fd5e7b8c9acd14cf32c473b315b
clami66 <claudio.mirabello@scilifelab.se>
2024-03-22 14:04:19 +0100

    code coverage test

--- run_test.sh
+++ run_test.sh
@@ -2,18 +2,19 @@
 set -euo pipefail
 
 # Test that cython version behaves the same as nocython
-DockQ examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb
-python src/DockQ/DockQ.py examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb
-DockQ examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb --no_align
-python src/DockQ/DockQ.py examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb --no_align
+coverage run -a -m DockQ.DockQ examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb
+coverage run -a src/DockQ/DockQ.py examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb
+coverage run -a -m DockQ.DockQ examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb --no_align
+coverage run -a src/DockQ/DockQ.py examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb --no_align
 # Multiple interfaces
-DockQ examples/dimer_dimer.model.pdb examples/dimer_dimer.pdb  --short
+coverage run -a -m DockQ.DockQ examples/dimer_dimer.model.pdb examples/dimer_dimer.pdb  --short
 # Test on structures with slightly different sequences
-DockQ examples/model.pdb examples/native.pdb --allowed_mismatches 1
+coverage run -a -m DockQ.DockQ examples/model.pdb examples/native.pdb --allowed_mismatches 1
 # Test various mapping strategies
-DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --short --mapping AB*:BA*
-DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --short --mapping :ABC
-DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --short --mapping ABCDEFGH:BADCFEHG
+coverage run -a -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --short --mapping AB*:BA*
+coverage run -a -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --short --mapping :ABC
+coverage run -a -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --short --mapping ABCDEFGH:BADCFEHG
 # Test that cif parsing behaves same as pdb parsing
-DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --mapping DH:AE
-DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB.cif.gz
+coverage run -a -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --mapping DH:AE
+coverage run -a -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB.cif.gz --mapping DH:AE
+coverage report
