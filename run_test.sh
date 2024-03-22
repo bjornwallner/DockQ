f2c86f8c8faecb9fd369e1d57e475d01cc91bfaa
clami66 <claudio.mirabello@scilifelab.se>
2024-03-22 13:21:27 +0100

    test script runs

--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 
 DockQ examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb
+python src/DockQ/DockQ.py examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb
 DockQ examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb --no_align
+python src/DockQ/DockQ.py examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb --no_align
 DockQ examples/model2.pdb examples/native2.pdb
 DockQ examples/dimer_dimer.model.pdb examples/dimer_dimer.pdb  --short
 DockQ examples/model.pdb examples/native.pdb --allowed_mismatches 1
