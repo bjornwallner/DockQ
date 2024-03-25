f7def987c309cef310556a1caa8adac2aeec37ef
clami66 <claudio.mirabello@scilifelab.se>
2024-03-25 11:53:31 +0100

    move includes to setup.cfg

--- run_test.sh
+++ run_test.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
 set -euo pipefail
 
-
-src=$(find $(dirname $(which DockQ))/../ -name "DockQ.py"  -exec dirname {} \;)
-echo $src
+rm -f .coverage
 # Test that cython version behaves the same as nocython
-coverage run -a --source='DockQ.DockQ' -m DockQ.DockQ examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb > test
+coverage run -m DockQ.DockQ examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb > test
 diff test testdata/1A2K.dockq
-coverage run -a --source=$src $src/DockQ.py examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb > test
+python src/DockQ/DockQ.py examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb > test
 diff test testdata/1A2K.dockq
-coverage run -a --source='DockQ.DockQ' -m DockQ.DockQ examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb --no_align > test
+coverage run -a -m DockQ.DockQ examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb --no_align > test
 diff test testdata/1A2K.dockq
-coverage run -a --source=$src $src/DockQ.py examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb --no_align > test
+python src/DockQ/DockQ.py examples/1A2K_r_l_b.model.pdb examples/1A2K_r_l_b.pdb --no_align > test
 diff test testdata/1A2K.dockq
 
 # Multiple interfaces
-coverage run -a --source='DockQ.DockQ' -m DockQ.DockQ examples/dimer_dimer.model.pdb examples/dimer_dimer.pdb  --short > test
+coverage run -a -m DockQ.DockQ examples/dimer_dimer.model.pdb examples/dimer_dimer.pdb  --short > test
 diff test testdata/dimer_dimer.dockq
 
 # Test on structures with slightly different sequences
-coverage run -a --source='DockQ.DockQ' -m DockQ.DockQ examples/model.pdb examples/native.pdb --allowed_mismatches 1 > test
+coverage run -a -m DockQ.DockQ examples/model.pdb examples/native.pdb --allowed_mismatches 1 > test
 diff test testdata/model.dockq
 
 # Test various mapping strategies
-coverage run -a --source='DockQ.DockQ' -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --short --mapping AB*:BA* > test
+coverage run -a -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --short --mapping AB*:BA* > test
 diff test testdata/1EXB_AB.BA.dockq
-coverage run -a --source='DockQ.DockQ' -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --short --mapping :ABC > test
+coverage run -a -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --short --mapping :ABC > test
 diff test testdata/1EXB_.ABC.dockq
-coverage run -a --source='DockQ.DockQ' -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --short --mapping ABCDEFGH:BADCFEHG > test
+coverage run -a -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --short --mapping ABCDEFGH:BADCFEHG > test
 diff test testdata/1EXB_ABCDEFGH.BADCFEHG.dockq
 
 # Test that cif parsing behaves same as pdb parsing
-coverage run -a --source='DockQ.DockQ' -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --mapping DH:AE > test
+coverage run -a -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB_r_l_b.pdb --mapping DH:AE > test
 diff test testdata/1EXB_DH.AE.dockq
-coverage run -a --source='DockQ.DockQ' -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB.cif.gz --mapping DH:AE > test
+coverage run -a -m DockQ.DockQ examples/1EXB_r_l_b.model.pdb examples/1EXB.cif.gz --mapping DH:AE > test
 diff test testdata/1EXB_DH.AE_cif.dockq
 
 coverage report
